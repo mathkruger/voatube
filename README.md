439c10755ca716490d5a0c00cfaeaf7ffac8e3f9
Matheus Kruger <matheus.silva@datapar.com.br>
2019-12-27 09:12:13 -0300

    ajustes

---
+++ README.md
@@ -0,0 +1,13 @@
+# VoaTube
+Se assim como eu você ama trabalhar enquanto vê vídeos, essa ideia é pra você. Eu sei que já existem milhares de apps que fazem a mesma coisa, mas o negócio aqui é aprendizado mesmo.
+ 
+Quer me ajudar nessa bagaça? Dá um fork ai e vamos deixar esse negócio muito louco!
+ 
+## Como testar? ##
+
+* Clone o repositório
+* Abra o Chrome e vá em Configurações -> Mais ferramentas -> Extensões.
+* Habilite o "modo desenvolvedor"
+* Clique em "Carregar extensão expandida..."
+* Vá até a pasta clonada e selecione-a.
+* Divirta-se!
---
+++ controller.js
@@ -0,0 +1,161 @@
+var VoaApp;
+
+(function ($, VoaApp) {
+    VoaApp = {
+        videoYoutube: document.getElementById('video_youtube'),
+        formVideo: document.getElementById('videoSubmit'),
+        formVideoList: document.getElementById('playlistSubmit'),
+        inputLink: document.getElementById('link'),
+        inputList: document.getElementById('links'),
+        close: document.getElementById('close'),
+        topBar: document.getElementById('topBar'),
+        logo: document.getElementById('logo')
+    };
+
+    VoaApp.listen = function () {
+        VoaApp.formVideo.addEventListener('submit', function (e) {
+            e.preventDefault();
+            openVideo(VoaApp.inputLink.value);
+        });
+
+        VoaApp.formVideoList.addEventListener('submit', function (e) {
+            e.preventDefault();
+            openPlaylist(VoaApp.inputList.value);
+        });
+
+        $(document).on('click', '.search-item', function(e) {
+            e.preventDefault();
+            openVideo($(this).attr('href'));
+            VoaApp.inputLink.value = $(this).attr('href');
+        })
+    };
+
+    VoaApp.init = function () {
+        VoaApp.listen();
+    };
+
+    function openPlaylist(link) {
+        $('#search-container').hide();
+
+        var links = link.split(',');
+        var geral = '';
+        var erro = false;
+        var primeiro = '';
+
+        for (var i = 0; i < links.length; i++) {
+            var regexYoutube = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+            var result = links[i].match(regexYoutube);
+
+            if (result && result[2].length == 11) {
+                if (i == 0) {
+                    primeiro = result[2];
+                }
+                else {
+                    geral += result[2];
+
+                    if (i < links.length - 1) {
+                        geral += ',';
+                    }
+                }
+            }
+            else {
+                erro = true;
+                break;
+            }
+        }
+
+        if (erro) {
+            alert("Ocorreu um erro ao carregar os vídeos da playlist!");
+        }
+        else {
+            VoaApp.videoYoutube.src = "https://www.youtube.com/embed/" + primeiro + "?autoplay=1&playlist=" + geral;
+
+            VoaApp.topBar.style.backgroundColor = '#404448';
+            VoaApp.logo.src = 'assets/youtube.png';
+            VoaApp.logo.style.display = 'block';
+        }
+    }
+
+    function openVideo(link) {
+        $('#search-container').hide();
+
+        var regexYoutube = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+        var result = link.match(regexYoutube);
+
+        if (result && result[2].length == 11) {
+
+            VoaApp.videoYoutube.src = "https://www.youtube.com/embed/" + result[2] + "?autoplay=1";
+
+            VoaApp.topBar.style.backgroundColor = '#404448';
+            //VoaApp.close.style.backgroundColor = '#E62117';
+            VoaApp.logo.src = 'assets/youtube.png';
+            VoaApp.logo.style.display = 'block';
+        }
+        else {
+            if (link.indexOf('twitch.tv') !== -1) {
+                var channelTwitch = link.split('/').pop();
+                if(link.indexOf("video") < 0) {
+                    VoaApp.videoYoutube.src = "http://player.twitch.tv/?channel=" + channelTwitch;
+                }
+                else {
+                    VoaApp.videoYoutube.src = "http://player.twitch.tv/?autoplay=false&video=" + channelTwitch;
+                }
+
+                VoaApp.topBar.style.backgroundColor = '#4B367C';
+                VoaApp.logo.src = 'assets/twitch.png';
+                VoaApp.logo.style.display = 'block';
+            }
+            else {
+                if(link.indexOf('mtv.com') > -1) {
+                    VoaApp.videoYoutube.src= link;
+                }
+                else {
+                    if(link.indexOf('globo') > -1) {
+                        VoaApp.videoYoutube.src = link;
+                    }
+                    else {
+                        search(link);
+                    }
+                }
+            }
+        }
+    }
+
+    function search(termo) {
+        var q = termo;
+        var $results = $('#search-container');
+        var url = "https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=30&key=AIzaSyBA3BhnVi5esgCGrncCnyviJmhzZV7WduA"
+
+        $.getJSON(url + "&q=" + q, function (json) {
+            var count = 0;
+
+            if (json.items) {
+
+                var items = json.items;
+                var html = '<ul class="list-group">';
+
+                items.forEach(function (item) {
+                    html += '<li class="list-group-item">';
+                    html += '<p><a class="search-item" href="http://youtu.be/' + item.id.videoId + '">';
+                    html += '<webview src="' + item.snippet.thumbnails.default.url + '" width="' + item.snippet.thumbnails.default.width + '" height="' + item.snippet.thumbnails.default.height + '" style="width: 120px !important; height: 90px !important;"></webview>';
+                    html += '<h5>' + item.snippet.title + '</h5></a></p>';
+                    html += '</li>';
+                    count++;
+                });
+
+                html += '</ul>'
+            }
+
+            $results.show();
+            if (count === 0) {
+                $results.html("No videos found");
+            } else {
+                $results.html(html);
+            }
+        });
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        VoaApp.init();
+    });
+}(jQuery, VoaApp));
